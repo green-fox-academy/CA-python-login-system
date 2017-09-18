a4589e6e9b1460513188e9f972377495b2f60912
gy0p4k <gyorgypakozdi@gmail.com>
2017-09-18 14:11:15 +0200

    feat(login): make registrate work

--- login.py
+++ login.py
@@ -1,16 +1,36 @@
+def get_usr_pass():
+    username = input("Username: ")
+    password = input("Password: ")
+    return {"username": username,
+            "password": password}
+
+
 def login():
-	print("login")
+    crededentials = get_usr_pass()
+
 
 def registrate():
-	print("registrate")
+    crededentials = get_usr_pass()
+    user_save(crededentials)
+
+
+def user_save(crededentials):
+    with open("database", "a") as db:
+        db.write(crededentials["username"] + ":" +
+                 crededentials["password"] + "\n")
+
+
+def choice_manager(choice):
+    return {
+        "0": login,
+        "1": registrate
+    }[choice]
+
 
 def login_engine():
-	choice = input("0: login, 1: registrate  ")
-	return {
-		"0": login,
-		"1": registrate
-	}[choice]
-			
+    try:
+        choice_manager(input("0: login, 1: registrate  "))()
+    except KeyError:
+        print("bad choice, dumbass")
 
-
-login_engine()()	
+login_engine()
